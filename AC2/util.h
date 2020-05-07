bbb47cdbae87534829668acb2e1920eefa1f2992
FJ-RATO <martinho.francisco@ua.pt>
2020-05-07 09:11:51 +0100

    G6 added

--- AC2/util.h
+++ AC2/util.h
@@ -6,3 +6,21 @@ void delay(unsigned int ms){
 		while(readCoreTimer() < PBCLK/1000);
 	}
 }
+
+void send2displays(unsigned char value){
+	static const char display7Scodes[] = {0x3F,0x06,0x5B,0x79,0x74,0x6D,0x6F,0x38,0x7F,0x7C,0x7E,0x67,0X0F,0x73,0x4F,0x4E};
+	static char flag = 1;
+	int dl = value & 0x0F;
+	int dh = value >> 4;
+	if(flag){
+		LATB = (LATB & 0x00FF) | (display7Scodes[dl] << 8);
+		LATDbits.LATD5 = 1;
+		LATDbits.LATD6 = 0;
+	}
+	else{
+		LATB = (LATB & 0x00FF) | (display7Scodes[dh] << 8);
+		LATDbits.LATD5 = 0;
+		LATDbits.LATD6 = 1;
+	}
+	flag = !flag;
+}
